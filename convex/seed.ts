3248163a6da885c1a3234e632dfd6a44d9d080af
Frank <frank.snelling03@gmail.com>
2026-03-22 11:33:00 +0000

    Change meme order

--- convex/seed.ts
+++ convex/seed.ts
@@ -1,7 +1,7 @@
 export const MEME_IMAGES: string[] = [
+  'https://storage.googleapis.com/random-file-storage/tense-table-smash.gif',
+  'https://storage.googleapis.com/random-file-storage/38iqvl.webp',
   'https://storage.googleapis.com/random-file-storage/Disaster-Girl.jpg',
   'https://storage.googleapis.com/random-file-storage/1c81c1.jpg',
   'https://storage.googleapis.com/random-file-storage/1p5q26.jpg',
-  'https://storage.googleapis.com/random-file-storage/38iqvl.webp',
-  'https://storage.googleapis.com/random-file-storage/tense-table-smash.gif',
 ]
